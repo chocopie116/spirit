c13acc793993a897cdc229b66904f73525a1b39f
chocopie116 <chocopie116@gmail.com>
2018-06-25 00:34:22 +0900

    iikanji

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,9 +1,27 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as firebase from 'firebase'
 
 import { Hello } from "./components/Hello";
+// ReactDOM.render(
+//     <Hello compiler="TypeScript" framework="React" />,
+//     document.getElementById("example")
+// );
+document.addEventListener('DOMContentLoaded', function () {
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // The Firebase SDK is initialized and available here!
+    //
+    // firebase.auth().onAuthStateChanged(user => { });
+    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+    // firebase.messaging().requestPermission().then(() => { });
+    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+    //
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
 
-ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
-);
+    try {
+        const app = firebase.app();
+        console.log(app);
+    } catch (e) {
+        console.error(e);
+    }
+});
